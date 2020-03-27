9d85859f9ac5f8fdeb079b67adb94d0ad76f7564
HenryTufano <rique_3l@hotmail.com>
2020-03-26 21:15:03 -0400

    atualização

--- ClassePessoa.py
+++ ClassePessoa.py
@@ -1,27 +1,27 @@
 class Pessoa(): 
-    def __init__(self):
-        self.nome=input("Digite o nome do Meliante: ")
-        self.idade=int(input("Digite a idade do Meliante: "))
-        self.peso=int(input("Digite p peso do Meliante: "))
-        self.altura=int(input("Digite a altura do Meliante: "))
-    def envelhecer(self):
-        self.t=int(input("Digite a quantidades de ano a ser envelhecido: "))
-        self.idade=self.t+self.idade
-        self.y=self.idade-21
-        self.x=self.t-self.y
-        if( self.x>=0):
-            self.altura=0.5*self.x+self.altura
-    def engordar(self):
-        self.g=int(input("Digite a quantidade de quilos a ser engordado: "))
-        self.peso=self.peso+self.g
-    def emagrecer(self):
-        self.e=int(input("Digite a quantidade de quilos a ser emagrecido: "))
-        self.peso=self.peso-self.e
-    def crescer(self):
-        self.c=int(input("Digite a quantidades de quilos a ser engordado: "))
-        self.altura=self.altura+self.c
-    def info(self):
-        print("O nome do meliante é : {}".format(self.nome))
-        print("A idade do meliante é : {}".format(self.idade))
-        print("O peso do meliante é : {} Kg".format(self.peso))
-        print("A altura do meliante é : {} cm".format(self.altura))
+    def __init__(self,nome,idade,peso,altura):
+        self.nome=nome
+        self.idade=idade 
+        self.peso=peso 
+        self.altura=altura
+    
+    def getNome(self):
+        return self.nome
+    def getIdade(self):
+        return self.idade
+    def getPeso(self):
+        return self.peso
+    def getAltura(self):
+        return self.altura
+    
+    def envelhecer(self,anos):
+        self.i=0
+        for self.i in range(anos):
+          if(anos<=21):
+            self.y=+1         
+            self.altura=0.5*self.y+self.altura
+    def engordar(self,kg):
+        self.peso=self.peso+kg
+    def emagrecer(self,kg):
+        self.peso=self.peso-kg
+    
--- pessoa.py
+++ pessoa.py
@@ -1,13 +1,25 @@
 from ClassePessoa import Pessoa
 op=5
-p=Pessoa()
+nome=input("Digite o nome do Meliante: ")
+idade=int(input("Digite a idade do Meliante: "))
+peso=int(input("Digite p peso do Meliante: "))
+altura=int(input("Digite a altura do Meliante: "))
+p=Pessoa(nome,idade,peso,altura)
+
 while op!=0:
     op=int(input(" [1] - envelhece \n [2] - engordar \n [3] - emagrecer \n [4] - Informação da pessoa \ndigite a opção: "))
     if op==1:
-        p.envelhecer()
+        anos = int(input("Digite a quantidades de ano a ser envelhecido: "))
+        p.envelhecer(anos)
+        print("A nova idade do meliante é:{0}".format(p.getIdade()))
     elif op==2:
-        p.engordar()
+        kg=int(input("Digite a quantidade de quilos a ser engordado: "))
+        p.engordar(kg)
     elif op==3:
-        p.emagrecer()
+        kg=int(input("Digite a quantidade de quilos a ser engordado: "))
+        p.emagrecer(kg)
     elif op==4:
-        p.info()
+         print("O nome: {0}".format(p.getNome()))
+         print("A idade é: {0} anos".format(p.getIdade()))
+         print("O peso é: {0} kg".format(p.getPeso()))
+         print("A altura é: {0} cm".format(p.getAltura()))
